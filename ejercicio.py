a711c764386e48f4c907646b8035a9adbe73cdcb
alx2704 <alx2704@gmail.com>
2025-04-24 21:39:01 -0500

    rev

---
+++ ejercicio.py
@@ -0,0 +1,10 @@
+n1 = int(input("ingrese el primer número"))
+n2 = int(input("ingrese el primer número"))
+suma = n1 + n2
+print(f"La suma es {suma:.2f}")
+#multiplicación
+multiplicacion = n1*n2
+print(f"El producto de {n1} y {n2} es: {multiplicacion:.2f}")
+#division
+division = n1/n2
+print(f"La division es: {division:.2f}")
